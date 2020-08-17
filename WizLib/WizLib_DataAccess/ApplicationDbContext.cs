3fcd6540eedb70effa1b5c2e4e6b24c8dba60e75
james-park-songtradr <james.park@songtradr.com>
2020-08-16 20:26:11 -0700

    add migrations and setting up database relationshisp

---
+++ WizLib/WizLib_DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WizLib_DataAccess.FluentConfig;
+using WizLib_Model.Models;
+
+namespace WizLib_DataAccess
+{
+    public class ApplicationDbContext : DbContext // : DbContext is using Microsoft.EntityFrameworkCore
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<Fluent_BookDetail> Fluent_BookDeatils { get; set; }
+        public DbSet<Fluent_Book> Fluent_Book { get; set; }
+        public DbSet<Fluent_Author> Fluent_Author { get; set; }
+        public DbSet<Fluent_Publisher> Fluent_Publisher { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //this is required for intermediary table or pivot table such as BookAuthor
+        {
+            //we configure fluent API
+            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.Author_Id, ba.Book_Id });
+
+            //category table name and column name
+            modelBuilder.Entity<Category>().ToTable("tbl_category");
+            modelBuilder.Entity<Category>().Property(c => c.Name).HasColumnName("CategoryName");
+
+            modelBuilder.ApplyConfiguration(new FluentBookConfig());
+            modelBuilder.ApplyConfiguration(new FluentBookDetailsConfig());
+            modelBuilder.ApplyConfiguration(new FluentBookAuthorConfig());
+            modelBuilder.ApplyConfiguration(new FluentPublisherConfig());
+            modelBuilder.ApplyConfiguration(new FluentAuthorConfig());
+            
+            /* The codes below have been replaced by lines 36-40
+            //BookDetails
+            modelBuilder.Entity<Fluent_BookDetail>().HasKey(b => b.BookDetail_Id);
+            modelBuilder.Entity<Fluent_BookDetail>().Property(b => b.NumberOfChapters).IsRequired();
+
+            //BookAuthor
+            //many to many relation
+            modelBuilder.Entity<Fluent_BookAuthor>().HasKey(ba => new { ba.Author_Id, ba.Book_Id });
+            modelBuilder.Entity<Fluent_BookAuthor>().HasOne(b => b.Fluent_Book).WithMany(b => b.Fluent_BookAuthors).HasForeignKey(b => b.Book_Id);
+            modelBuilder.Entity<Fluent_BookAuthor>().HasOne(b => b.Fluent_Author).WithMany(b => b.Fluent_BookAuthors).HasForeignKey(b => b.Author_Id);
+
+            //Book
+            modelBuilder.Entity<Fluent_Book>().HasKey(b => b.Book_Id);
+            modelBuilder.Entity<Fluent_Book>().Property(b => b.ISBN).IsRequired().HasMaxLength(15);
+            modelBuilder.Entity<Fluent_Book>().Property(b => b.Title).IsRequired();
+            modelBuilder.Entity<Fluent_Book>().Property(b => b.Price).IsRequired();
+            //one to one relation between book and book detail
+            modelBuilder.Entity<Fluent_Book>().HasOne(b => b.Fluent_BookDetail).WithOne(b => b.Fluent_Book).HasForeignKey<Fluent_Book>("BookDetail_Id");
+            //one to many relation between book and publisher
+            modelBuilder.Entity<Fluent_Book>().HasOne(b => b.Fluent_Publisher).WithMany(b => b.Fluent_Book).HasForeignKey(b => b.Publisher_Id);
+
+            //Author
+            modelBuilder.Entity<Fluent_Author>().HasKey(b => b.Author_Id);
+            modelBuilder.Entity<Fluent_Author>().Property(b => b.FirstName).IsRequired().HasMaxLength(15);
+            modelBuilder.Entity<Fluent_Author>().Property(b => b.LastName).IsRequired();
+            modelBuilder.Entity<Fluent_Author>().Ignore(b => b.FullName);
+
+            //Publisher
+            modelBuilder.Entity<Fluent_Publisher>().HasKey(b => b.Publisher_Id);
+            modelBuilder.Entity<Fluent_Publisher>().Property(b => b.Name).IsRequired();
+            modelBuilder.Entity<Fluent_Publisher>().Property(b => b.Location).IsRequired();
+            */
+        }
+    }
+}
---
+++ WizLib/WizLib_DataAccess/Migrations/20200816022357_changeNameToGenreNameinGenreTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib_DataAccess.Migrations
+{
+    public partial class changeNameToGenreNameinGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GenreName",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql("UPDATE dbo.genres SET GenreName=Name"); //this copies data in Name column to GenreNames column
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Genres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql("UPDATE dbo.genres SET Name=GenreName"); //this copies data in Name column to GenreNames column
+
+            migrationBuilder.DropColumn(
+                name: "GenreName",
+                table: "Genres");
+        }
+    }
+}
